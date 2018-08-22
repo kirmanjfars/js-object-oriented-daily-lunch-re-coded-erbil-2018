589cb1429e08b8612fdf22a32d70585183357ee4
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-22 10:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,9 +72,9 @@ class Customer{
     }
     
     totalSpent(){
-      return this.meals.reduce(function(ac, e){
-         return  ac += e.price;
-      }, 0);
+      return this.meals.reduce((ac, e) =>
+           ac += e.price
+      , 0);
       
     }
 }
